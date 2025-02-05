e24763b090f32b83118af32f70bf8125dbd6d1a8
AlgoRythm-Dylan <dperunko@comcast.net>
2025-02-05 16:03:49 -0500

    Most table structures complete

--- src/SQL/create-tables.sql
+++ src/SQL/create-tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE PersonalSite.PaletteColors (
 	Green TINYINT UNSIGNED NOT NULL,
 	Blue TINYINT UNSIGNED NOT NULL,
     
-    CONSTRAINT PaletteColorsUnique UNIQUE (Red, Green, Blue)
+    CONSTRAINT C_PaletteColorsUnique UNIQUE (Red, Green, Blue)
 );
 
 CREATE INDEX IDX_PaletteColorsSpacial ON PersonalSite.PaletteColors (Red, Green, Blue);
@@ -18,5 +18,44 @@ CREATE TABLE PersonalSite.Images (
     CaptureDate DATETIME DEFAULT NOW(),
     
     UploadDate DATETIME DEFAULT NOW(),
-    DeleteDate DATETIME DEFAULT NULL
+    DeletedDate DATETIME DEFAULT NULL
 ) CHARACTER SET utf8mb4;
+
+CREATE TABLE PersonalSite.ImageColors (
+    ImageID INT NOT NULL,
+    PaletteColorID INT NOT NULL,
+
+    CONSTRAINT FK_ImageColors_R_Images
+        FOREIGN KEY (ImageID) REFERENCES PersonalSite.Images(ID)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_ImageColors_R_Colors FOREIGN KEY (PaletteColorID)
+        REFERENCES PersonalSite.PaletteColors(ID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE PersonalSite.Subjects (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    ParentID INT DEFAULT NULL,
+    Name VARCHAR(128) CHARACTER SET utf8mb4,
+
+    CreatedDate DATETIME DEFAULT NOW(),
+    DeletedDate DATETIME DEFAULT NULL,
+
+    CONSTRAINT FK_Subjects_R_Parent
+        FOREIGN KEY (ParentID) REFERENCES PersonalSite.Subjects(ID)
+        ON DELETE CASCADE
+) CHARACTER SET utf8mb4;
+
+CREATE TABLE PersonalSite.ImageSubjects (
+    ImageID INT NOT NULL,
+    SubjectID INT NOT NULL,
+    QuantityBucket INT NOT NULL,
+    IsPrimarySubject TINYINT NOT NULL,
+
+    CONSTRAINT FK_ImgSubj_R_Image
+        FOREIGN KEY (ImageID) REFERENCES PersonalSite.Images(ID)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_ImgSubj_R_Subjects
+        FOREIGN KEY (SubjectID) REFERENCES PersonalSite.Subjects(ID)
+        ON DELETE CASCADE
+);
