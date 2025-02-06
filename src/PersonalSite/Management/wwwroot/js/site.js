1d58461f6de44b4b24397b76a02ce80e889cc04c
AlgoRythm-Dylan <dperunko@comcast.net>
2025-02-06 15:58:05 -0500

    More appealing style

--- src/PersonalSite/Management/wwwroot/js/site.js
+++ src/PersonalSite/Management/wwwroot/js/site.js
@@ -1,13 +1,24 @@
 ﻿addEventListener("load", () => {
     document.querySelectorAll(".image-upload-area").forEach(el => {
+        const relatedElement = document.querySelector(el.getAttribute("data-related-input"));
         el.addEventListener("click", () => {
-            const relatedElement = document.querySelector(el.getAttribute("data-related-input"));
             if (relatedElement) {
                 relatedElement.click();
             }
             else {
                 throw new Error("Cannot find related element");
             }
+        });
+        el.addEventListener("dragover", e => {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+        el.addEventListener("drop", e => {
+            e.preventDefault();
+            e.stopPropagation();
+            relatedElement.files = e.dataTransfer.files;
+            relatedElement.dispatchEvent(new Event('change', { bubbles: true }));
+            relatedElement.dispatchEvent(new Event('input', { bubbles: true }));
         });
     });
 });
