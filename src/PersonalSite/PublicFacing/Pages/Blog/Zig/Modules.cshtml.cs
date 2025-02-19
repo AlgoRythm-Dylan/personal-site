670a6654760a60c50d7c25db36e07845168de27a
AlgoRythm-Dylan <dperunko@comcast.net>
2025-02-19 11:21:22 -0500

    First blog post

---
+++ src/PersonalSite/PublicFacing/Pages/Blog/Zig/Modules.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PublicFacing.Pages.Blog.Zig
+{
+    public class ModulesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
