e6d0398c2a0571633732f71fec23a47a9ed53c8c
AlgoRythm-Dylan <dperunko@comcast.net>
2025-04-29 00:43:34 -0400

    some changes

--- src/PersonalSite/Web/Pages/Index.cshtml.cs
+++ src/PersonalSite/Web/Pages/Index.cshtml.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Lib.Services.Spec;
 
 namespace Web.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IObjectViewService ObjectViewService;
+        public IndexModel(IObjectViewService ovs)
+        {
+            ObjectViewService = ovs;
+        }
 
+        public async void OnGet()
+        {
+            await ObjectViewService.RecordPageViewAsync("Index");
+        }
     }
 }
